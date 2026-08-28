a1822e3f2ddff1ec8d0bb1d38e3bf7a83dbc8653
HRAshton <12210721+hrashton@users.noreply.github.com>
2026-08-28 22:47:00 +0200

    refactor(http): Rename wire model to protocol

--- adapters/http-example/src/index.ts
+++ adapters/http-example/src/index.ts
@@ -2,5 +2,5 @@ export type { HttpRemotishAdapterOptions } from './http-adapter.js';
 export { HttpRemotishAdapter } from './http-adapter.js';
 export type { HeaderProvider, HttpClientOptions } from './http-client.js';
 export { HttpClient } from './http-client.js';
-export type { WireChange, WireCommitRequest } from './wire.js';
-export { encodeCommitRequest } from './wire.js';
+export type { WireChange, WireCommitRequest } from './protocol.js';
+export { encodeCommitRequest } from './protocol.js';
